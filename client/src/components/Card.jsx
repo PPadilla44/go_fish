d88e633d4ca2aa8e5b483cf07ed4e3396e7d10e9
Pablo Padilla <Padilla.Pablo1998@gmail.com>
2022-01-08 16:22:59 -0800

    added suits and values

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
 import { useRef } from 'react';
+import { connect } from 'react-redux';
+import { setCard } from '../store/utils/thunkCreators';
+import CardFace from './CardFace';
 
 const Card = (props) => {
 
-    const { pointVal, suit, rotation } = props;
+    const { data, rotation, setCard } = props
+    const { point_val: pointVal, suit } = data;
+
+    const suits = {
+        "spades": "♠︎",
+        "hearts": "♥︎",
+        "clubs": "♣︎",
+        "diamonds": "♦︎"
+    };
+
+
     const ref = useRef(null)
 
     const styles = {
@@ -14,7 +27,7 @@ const Card = (props) => {
 
 
     const onHover = (e) => {
-        if(ref.current.style.transform.length < 20) {
+        if (ref.current.style.transform.length < 20) {
             ref.current.style.transform += `translateY(-15px)`
         }
     }
@@ -24,12 +37,35 @@ const Card = (props) => {
     }
 
     return (
-        <div className='card' ref={ref} style={styles.card} onMouseOver={onHover} onMouseLeave={offHover}>
-            <h4>{pointVal}</h4>
-            <h4>{suit}</h4>
+        <div className='card'
+            ref={ref}
+            style={styles.card}
+            onMouseOver={onHover}
+            onMouseLeave={offHover}
+            onClick={() => setCard(data)}
+        >
+            <CardFace
+                top={true}
+                pointVal={pointVal}
+                suit={suits[suit]}
+            />
+            <CardFace
+                top={false}
+                pointVal={pointVal}
+                suit={suits[suit]}
+            />
+
         </div>
     )
 }
 
+const mapDispatchToProps = (dispatch) => {
+    return {
+        setCard: (card) => {
+            dispatch(setCard(card))
+        }
+    }
+}
 
-export default Card
+
+export default connect(null, mapDispatchToProps)(Card);
